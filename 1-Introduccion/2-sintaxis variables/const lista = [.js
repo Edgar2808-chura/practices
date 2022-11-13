e3899f5f157a23d7e4f277c583c8691da6675ce1
Edgar Chura Humora <113655611+Edgar2808-chura@users.noreply.github.com>
2022-11-13 17:37:53 -0500

    Add files via upload

---
+++ 1-Introduccion/2-sintaxis variables/const lista = [.js
@@ -0,0 +1,16 @@
+const lista = [
+    nombre= "Edgar", 
+    eddad= 26, 
+    desarrollador =true,
+    fechaNac = new Date(1996, 07, 28),
+    libroFavorito ={ 
+        titulo: "Fire & Blood",
+        autor: "George R. R. Martin",
+        fecha: new Date(2018, 10, 28),
+        url: "https://a.co/d/1k4Vg7I"
+    },
+
+];
+
+console.log(lista)
+console.log(libroFavorito)
